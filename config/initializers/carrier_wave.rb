69f445f1425e17ab2274e021c2660b4828e94104
floalex <flo.liang@my.com>
2017-12-13 01:08:36 +0000

    Temporarily remove CarrierWave production setting

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,19 +1,18 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
-    config.storage = :aws
-    config.aws_bucket = 'S3_BUCKET_NAME'
-    config.aws_acl    = 'public-read'
+    # config.storage = :aws
+    # config.aws_bucket = 'S3_BUCKET_NAME'
+    # config.aws_acl    = 'public-read'
 
-    # The maximum period for authenticated_urls is only 7 days.
-    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    # # The maximum period for authenticated_urls is only 7 days.
+    # config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
     
-    config.aws_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'xxx',                        # required
-      aws_secret_access_key: 'yyy',                        # required
-      region:                'eu-west-1',                  # required
-    }
-
+    # config.aws_credentials = {
+    #   provider:              'AWS',                        # required
+    #   aws_access_key_id:     'xxx',                        # required
+    #   aws_secret_access_key: 'yyy',                        # required
+    #   region:                'eu-west-1',                  # required
+    # }
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?

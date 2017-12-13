38cdf4764f644399ae943db1d5b36de90f187bd4
floalex <flo.liang@my.com>
2017-12-13 01:03:49 +0000

    remove fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,19 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
-    config.storage = :fog
-    config.fog_credentials = {
+    config.storage = :aws
+    config.aws_bucket = 'S3_BUCKET_NAME'
+    config.aws_acl    = 'public-read'
+
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    
+    config.aws_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id:     'xxx',                        # required
       aws_secret_access_key: 'yyy',                        # required
-      region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+      region:                'eu-west-1',                  # required
     }
-    config.fog_directory  = 'name_of_directory'                          # required
+
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
